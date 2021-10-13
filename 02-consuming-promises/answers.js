b30448791018c871e497aee304ebab571be033b6
oridavidi100 <89572548+oridavidi100@users.noreply.github.com>
2021-10-13 14:44:30 +0300

    02-consuming-promises

--- 02-consuming-promises/answers.js
+++ 02-consuming-promises/answers.js
@@ -7,8 +7,9 @@
  * 
  */
 function waitForPromise(promise, action){
-  /* IMPLEMENT ME */
+promise.then(action)
 }
+
 /**
  * 
  * EXERCISE 2
@@ -18,7 +19,9 @@ function waitForPromise(promise, action){
  * @param {handler} handler 
  */
 function consumePromise(promise, consumer, handler){
-  /* IMPLEMENT ME! */
+  promise
+  .then(consumer)
+  .catch(handler)
 }
 
 /**
